511681b16f13923e65a11c5d82bab8c1ef5483c3
mkfreeman <michael.k.freeman@gmail.com>
2019-02-12 12:58:44 -0800

    Updates

---
+++ precache-manifest.39fdc745bf5e5422f3163a051750db1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba5015106923cf03906c",
+    "url": "/binary-predictions/static/js/runtime~main.ba501510.js"
+  },
+  {
+    "revision": "297de8c2a4d4d3e9ddc4",
+    "url": "/binary-predictions/static/js/main.297de8c2.chunk.js"
+  },
+  {
+    "revision": "6b87f01175b84921efad",
+    "url": "/binary-predictions/static/js/2.6b87f011.chunk.js"
+  },
+  {
+    "revision": "297de8c2a4d4d3e9ddc4",
+    "url": "/binary-predictions/static/css/main.6d1ccdb5.chunk.css"
+  },
+  {
+    "revision": "83e821d8cd9c023034833a5c27f8b2ae",
+    "url": "/binary-predictions/index.html"
+  }
+];
